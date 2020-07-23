88c33464551db19e8246462348523e72d6ef8563
Rafiat Aminu <rafiat.aminu@etranzactng.com>
2020-07-23 14:11:37 +0100

    intern

---
+++ src/main/java/com/etz/DataJPA/DataJpaApplication.java
@@ -0,0 +1,13 @@
+package com.etz.DataJPA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataJpaApplication.class, args);
+	}
+
+}
